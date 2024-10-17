1cbd7cdbba40ead160cef7ef975b2eb169fd2526
huyDev <hyperrhy1412@gmail.com>
2024-10-17 08:20:48 +0700

    Promotion repo

--- src/Repository/IPromotionRepository.java
+++ src/Repository/IPromotionRepository.java
@@ -1,4 +1,13 @@
 package Repository;
 
-public interface IPromotionRepository extends Repository{
+import Model.Customer;
+
+import java.util.TreeSet;
+
+public interface IPromotionRepository extends Repository<Customer, TreeSet<Customer>>{
+    final String promotionPath = "\\Data\\promotion.csv";
+
+    public TreeSet<Customer> readFile();
+
+    public void writeFile(TreeSet<Customer> customers);
 }
--- src/Repository/PromotionRepository.java
+++ src/Repository/PromotionRepository.java
@@ -1,4 +1,20 @@
 package Repository;
 
-public class PromotionRepository {
+import Model.Customer;
+
+import java.util.TreeSet;
+
+public class PromotionRepository implements IPromotionRepository{
+
+    @Override
+    public TreeSet<Customer> readFile() {
+        return null;
+    }
+
+
+    @Override
+    public void writeFile(TreeSet<Customer> customers) {
+
+    }
+
 }
